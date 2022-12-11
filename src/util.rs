8e075175dcc3065cc7e1512b17c6efd484fd803c
unknown <matija.ojo@gmail.com>
2022-12-11 16:22:49 +0100

    Remove unused code

--- src/util.rs
+++ src/util.rs
@@ -16,35 +16,6 @@ pub fn rotate_point_around_center(point: glam::Vec2, center: glam::Vec2, angle: 
     glam::vec2(r_x, r_y)
 }
 
-pub fn get_arc_points(
-    center: glam::Vec2,
-    radii: glam::Vec2,
-    start_angle: f32,
-    sweep_angle: f32,
-    x_rotation: f32,
-) -> Vec<glam::Vec2> {
-    let arc = lyon_geom::Arc {
-        center: lyon_geom::euclid::Point2D {
-            x: center.x,
-            y: center.y,
-            _unit: std::marker::PhantomData,
-        },
-        radii: lyon_geom::euclid::Vector2D {
-            x: radii.x,
-            y: radii.y,
-            _unit: std::marker::PhantomData,
-        },
-        start_angle: lyon_geom::Angle::radians(start_angle),
-        sweep_angle: lyon_geom::Angle::radians(sweep_angle),
-        x_rotation: lyon_geom::Angle::radians(x_rotation),
-    };
-
-    arc.flattened(0.01)
-        .into_iter()
-        .map(|p| glam::vec2(p.x, p.y))
-        .collect::<Vec<glam::Vec2>>()
-}
-
 #[inline]
 pub fn check_collision(t1: &TransformComponent, t2: &TransformComponent) -> bool {
     let distance_vec = t1.position - t2.position;
